60f7cfe17d3dbb978d1fa8d9e568184bc74e4b33
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-10-12 20:09:54 -0700

    adds indentation and line number configs

--- lua/juscuzryancan/core/init.lua
+++ lua/juscuzryancan/core/init.lua
@@ -1,4 +1,17 @@
 require("juscuzryancan.core.keymaps")
 
+-- colorscheme --
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
+
+-- indentation --
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+-- line number settings --
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.numberwidth = 2
+
+-- vim clipboard --
+-- vim.o.clipboard = 
