ba5e23e480afbf62e9e01a38697f6015e21a8741
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-10-20 13:05:51 -0700

    creates lualine config

--- lua/juscuzryancan/plugins/lualine.lua
+++ lua/juscuzryancan/plugins/lualine.lua
@@ -1,3 +1,9 @@
 return {
-	'nvim-lualine/lualine.nvim'
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	options = { theme = 'gruvbox' },
+	config = function()
+		local lualine = require('lualine')
+		lualine.setup()
+	end
 }
