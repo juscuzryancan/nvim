31aae83e88fe6ddcc0b7814c8e297f036bc88f02
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-10-29 22:40:43 -0500

    linting

--- lua/juscuzryancan/lazy.lua
+++ lua/juscuzryancan/lazy.lua
@@ -1,28 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{import = "juscuzryancan.plugins"}
+	{ import = "juscuzryancan.plugins" },
+	{ import = "juscuzryancan.plugins.lsp" },
 }, {
 	install = {
-		colorscheme = {"gruvbox"}
+		colorscheme = { "gruvbox" },
 	},
 	checker = {
 		enabled = true,
-		notify = false
+		notify = false,
 	},
 	change_detection = {
-		notify = false
-	}
+		notify = false,
+	},
 })
