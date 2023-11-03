d6d42c925a86c9bf47698a036eeb6ad7fad8512e
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-03 12:24:11 -0500

    adds whichkey descs for save and close buffer

--- lua/juscuzryancan/core/keymaps.lua
+++ lua/juscuzryancan/core/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", ":w!<cr>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>w", ":w!<cr>", { noremap = true, silent = false, desc = "Save Current Buffer" })
+vim.keymap.set("n", "<leader>c", ":wq<cr>", { noremap = true, silent = false, desc = "Close Current Buffer" })
