c5996576b72c323b097ca0d9cecc0f743af68ffc
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-07 09:49:26 -0600

    edits qa mapping

--- lua/juscuzryancan/core/keymaps.lua
+++ lua/juscuzryancan/core/keymaps.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w!<cr>", { noremap = true, silent = false, desc = "Save Current Buffer" })
 vim.keymap.set("n", "<leader>c", ":wq<cr>", { noremap = true, silent = false, desc = "Close Current Buffer" })
-vim.keymap.set("n", "<leader>wqa", ":w!<cr>", { noremap = true, silent = false, desc = "Save and Close All" })
+vim.keymap.set("n", "<leader>qa", ":wqa<cr>", { noremap = true, silent = false, desc = "Save and Close All" })
