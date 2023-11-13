8478966bbb13b2a1d894ee301838679e1681ae28
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-13 15:56:50 -0800

    uses conform to apply formatters

---
+++ lua/juscuzryancan/plugins/formatting.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
