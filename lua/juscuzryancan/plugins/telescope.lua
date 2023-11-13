f62ca74585679b4fc1241f1e28d1899b51ce3093
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-13 15:47:18 -0800

    adds live_grep keymap

--- lua/juscuzryancan/plugins/telescope.lua
+++ lua/juscuzryancan/plugins/telescope.lua
@@ -1,9 +1,16 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.4",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim", "ThePrimeagen/harpoon" },
 	config = function()
+		local telescope = require("telescope")
+
+		-- Harpoon Support --
+		telescope.load_extension("harpoon")
+
+		-- Keymappings --
 		local keymap = vim.keymap
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+		keymap.set("n", "<leader>sf", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
+		keymap.set("n", "<leader>st", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
 	end,
 }
