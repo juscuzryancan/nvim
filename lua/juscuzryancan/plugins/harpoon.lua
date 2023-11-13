4aefcdda13c7b347652883ba8c8f01cc593352b8
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-13 15:48:17 -0800

    adds harpoon keymappings

--- lua/juscuzryancan/plugins/harpoon.lua
+++ lua/juscuzryancan/plugins/harpoon.lua
@@ -1,3 +1,23 @@
 return {
 	"ThePrimeagen/harpoon",
+	config = function()
+		local mark = require("harpoon.mark")
+		local ui = require("harpoon.ui")
+
+		vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Harpoon Add File" })
+		vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu, { desc = "Harpoon Quick Menu" })
+
+		vim.keymap.set("n", "<C-1>", function()
+			ui.nav_file(1)
+		end, { desc = "Harpoon File 1" })
+		vim.keymap.set("n", "<C-2>", function()
+			ui.nav_file(2)
+		end, { desc = "Harpoon File 2" })
+		vim.keymap.set("n", "<C-3>", function()
+			ui.nav_file(3)
+		end, { desc = "Harpoon File 3" })
+		vim.keymap.set("n", "<C-4>", function()
+			ui.nav_file(4)
+		end, { desc = "Harpoon File 4" })
+	end,
 }
