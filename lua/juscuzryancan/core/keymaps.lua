f5e3f6767477664ff1358ca773b893a49f7b81e1
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-11-12 21:05:59 -0800

    adds extra keymaps

--- lua/juscuzryancan/core/keymaps.lua
+++ lua/juscuzryancan/core/keymaps.lua
@@ -1,4 +1,31 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", ":w!<cr>", { noremap = true, silent = false, desc = "Save Current Buffer" })
-vim.keymap.set("n", "<leader>c", ":wq<cr>", { noremap = true, silent = false, desc = "Close Current Buffer" })
-vim.keymap.set("n", "<leader>qa", ":wqa<cr>", { noremap = true, silent = false, desc = "Save and Close All" })
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+--
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<leader>w", ":w!<cr>", { noremap = true, silent = false, desc = "Save Current Buffer" })
+keymap("n", "<leader>c", ":wq<cr>", { noremap = true, silent = false, desc = "Close Current Buffer" })
+keymap("n", "<leader>qa", ":wqa<cr>", { noremap = true, silent = false, desc = "Save and Close All" })
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Window Movement
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Navigate buffers
+keymap("n", "<S-k>", ":bnext<CR>", opts)
+keymap("n", "<S-j>", ":bprevious<CR>", opts)
